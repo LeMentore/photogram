8269f1087ced870db5accdbb0e1182ebd031f5b0
Charlie Frye <klayton.alan@gmail.com>
2018-03-01 15:53:06 +0500

    download picture

--- src/PreviewModal.js
+++ src/PreviewModal.js
@@ -5,12 +5,13 @@ export default class PreviewModal extends Component{
         const { selectedPic, isOpen, close } = this.props
         const modalClass = isOpen ? '-show' : ''
         const previewLink = `${selectedPic.pic}?fit=crop&w=1920&h=1080`
+        const downloadLink = 'https://unsplash.com' + selectedPic.link.replace('?photo=', 'photos/') + '/download?force=true'
 
         return(
             <div className={`preview-modal ${modalClass}`}>
                 <span className="close-icon" onClick={close}>&times;</span>
                 <div className="preview-modal-inner" style={{backgroundImage: `url(${previewLink})` }}>
-                    <a href="#" className="btn download-btn">Download</a>
+                    <a href={downloadLink} className="btn download-btn">Download</a>
                 </div>
             </div>
         )
