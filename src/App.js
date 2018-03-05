9dfaeb2349fd0282efbe145ead26083f14ccdb3c
Charlie Frye <klayton.alan@gmail.com>
2018-03-05 14:22:25 +0500

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Gallery from './Gallery'
 
 class App extends Component {
   render() {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <Gallery />
-        </HashRouter>
+        </BrowserRouter>
     )
   }
 }
